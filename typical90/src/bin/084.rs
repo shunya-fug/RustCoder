242a3fff0ced323da09560f1a5e5528fe61af0d2
shunya-fug <shunya0715@gmail.com>
2022-05-08 16:47:26 +0900

    solve: typical90_cf
    https://atcoder.jp/contests/typical90/tasks/typical90_cf

--- typical90/src/bin/084.rs
+++ typical90/src/bin/084.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn rle(s: Vec<char>) -> Vec<(char, usize)> {
+    let mut res: Vec<(char, usize)> = Vec::new();
+    let mut pre = ' ';
+    let mut chain = 1;
+    for c in s.into_iter() {
+        if c == pre {
+            chain += 1;
+        } else {
+            if pre != ' ' {
+                res.push((pre, chain));
+            }
+            pre = c;
+            chain = 1;
+        }
+    }
+    res.push((pre, chain));
+    res
+}
+
 fn main() {
-    todo!();
+    input! {n: usize, s: Chars}
+    let s = rle(s);
+
+    let ret: usize = s.into_iter().map(|(_, n)| n * (n + 1) / 2).sum();
+    let ans = n * (n + 1) / 2 - ret;
+
+    println!("{}", ans);
 }
