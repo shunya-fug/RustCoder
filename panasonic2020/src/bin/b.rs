a246b53a2c532c70f5173cf87735d7b0d8b51feb
shunya-fug <shunya0715@gmail.com>
2022-05-08 18:18:48 +0900

    solve: panasonic2020_b
    https://atcoder.jp/contests/panasonic2020/tasks/panasonic2020_b

--- panasonic2020/src/bin/b.rs
+++ panasonic2020/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {(h, w): (usize, usize)}
+
+    let ans = if h == 1 || w == 1 {
+        1
+    } else {
+        let mut ans = w * h / 2;
+        if h % 2 == 1 && w % 2 == 1 {
+            ans += 1;
+        }
+        ans
+    };
+
+    println!("{}", ans);
 }
