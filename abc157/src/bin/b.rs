d8045897053e9b097fab0096473e91de398404b1
shunya-fug <shunya0715@gmail.com>
2022-05-08 19:58:10 +0900

    solve: abc157_b

--- abc157/src/bin/b.rs
+++ abc157/src/bin/b.rs
@@ -1,6 +1,36 @@
+use ndarray::{Array, Array2, Axis};
 use proconio::{fastout, input};
+
+fn bingo(a: &Array2<usize>) -> bool {
+    if a.axis_iter(Axis(0)).any(|row| row.iter().all(|x| *x == 0)) {
+        return true;
+    }
+    if a.axis_iter(Axis(1)).any(|col| col.iter().all(|x| *x == 0)) {
+        return true;
+    }
+    if a.diag().iter().all(|x| *x == 0) {
+        return true;
+    }
+    if a[[0, 2]] == 0 && a[[1, 1]] == 0 && a[[2, 0]] == 0 {
+        return true;
+    }
+
+    false
+}
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        a: [[usize; 3]; 3],
+        n: usize,
+        b: [usize; n],
+    }
+    let mut a: Array2<usize> =
+        Array::from_shape_vec((3, 3), a.into_iter().flatten().collect()).unwrap();
+
+    for b in b {
+        a.mapv_inplace(|x| if x == b { 0 } else { x });
+    }
+
+    println!("{}", if bingo(&a) { "Yes" } else { "No" });
 }
