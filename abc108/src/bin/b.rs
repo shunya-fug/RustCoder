6054c0545e1927518ce5bcb3fdfb398155be07f7
shunya-fug <shunnya0715@gmail.com>
2022-05-10 18:02:46 +0900

    solve: abc108_b

--- abc108/src/bin/b.rs
+++ abc108/src/bin/b.rs
@@ -2,5 +2,15 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        p1: (isize, isize),
+        p2: (isize, isize),
+    }
+
+    let dx = p2.0 - p1.0;
+    let dy = p2.1 - p1.1;
+
+    let ans = format!("{} {} {} {}", p2.0 - dy, p2.1 + dx, p1.0 - dy, p1.1 + dx);
+
+    println!("{}", ans);
 }
