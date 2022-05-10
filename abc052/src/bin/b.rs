4892618c07f578de9395e794064847c7ba53e3c0
shunya-fug <shunnya0715@gmail.com>
2022-05-10 18:26:04 +0900

    solve: abc052_b

--- abc052/src/bin/b.rs
+++ abc052/src/bin/b.rs
@@ -1,6 +1,18 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {_: usize, s: Chars}
+    let mut x = vec![0];
+
+    for c in s {
+        let current = *x.last().unwrap();
+        match c {
+            'I' => x.push(current + 1),
+            'D' => x.push(current - 1),
+            _ => continue,
+        }
+    }
+
+    println!("{}", x.iter().max().unwrap());
 }
