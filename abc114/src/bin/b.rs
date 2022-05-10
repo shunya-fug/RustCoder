c03e30c1b6d03b36e09077be3f26a521b079512c
shunya-fug <shunnya0715@gmail.com>
2022-05-10 17:31:14 +0900

    solve: abc114_b

--- abc114/src/bin/b.rs
+++ abc114/src/bin/b.rs
@@ -1,6 +1,16 @@
+use proconio::marker::Chars;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {s: Chars}
+    const TARGET: usize = 753;
+
+    let mut ans = std::usize::MAX;
+    for chs in s.windows(3) {
+        let n = chs.iter().collect::<String>().parse::<usize>().unwrap();
+        ans = ans.min(n.max(TARGET) - n.min(TARGET));
+    }
+
+    println!("{}", ans);
 }
