5166d054b72d4da0d69a9b5f1bcb682628d776a6
shunya-fug <shunnya0715@gmail.com>
2022-05-10 16:17:54 +0900

    solve: abc092_b

--- abc092/src/bin/b.rs
+++ abc092/src/bin/b.rs
@@ -2,5 +2,21 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, d, x): (usize, usize, usize),
+        a: [usize; n],
+    }
+
+    let mut count = x;
+    for a in a {
+        let mut next = 0;
+        for d in 0..d {
+            if d == next {
+                count += 1;
+                next += a;
+            }
+        }
+    }
+
+    println!("{}", count);
 }
