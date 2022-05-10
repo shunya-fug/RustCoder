f9001323e54cdff0f670b83ca61a3daf72400305
shunya-fug <shunnya0715@gmail.com>
2022-05-10 16:35:19 +0900

    solve: hitachi2020_b

--- hitachi2020/src/bin/b.rs
+++ hitachi2020/src/bin/b.rs
@@ -1,6 +1,25 @@
-use proconio::{fastout, input};
+use proconio::{derive_readable, fastout, input};
+
+#[derive_readable]
+struct Coupon {
+    x: usize,
+    y: usize,
+    c: usize,
+}
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, m): (usize, usize, usize),
+        pa: [usize; a],
+        pb: [usize; b],
+        coupon: [Coupon; m],
+    }
+
+    let mut ans = pa.iter().min().unwrap() + pb.iter().min().unwrap();
+    for coupon in coupon {
+        ans = ans.min(pa[coupon.x - 1] + pb[coupon.y - 1] - coupon.c);
+    }
+
+    println!("{}", ans);
 }
