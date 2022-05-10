24a98d7bf93665b06e985e97a159787dbea4a5da
shunya-fug <shunnya0715@gmail.com>
2022-05-10 16:00:50 +0900

    solve: agc027_a

--- agc027/src/bin/a.rs
+++ agc027/src/bin/a.rs
@@ -2,5 +2,23 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, mut x): (usize, usize),
+        mut a: [usize; n],
+    }
+
+    a.sort_unstable();
+    let mut count = 0;
+    for a in &a[..n - 1] {
+        if *a > x {
+            break;
+        }
+        x -= a;
+        count += 1;
+    }
+    if a.last().unwrap() == &x {
+        count += 1;
+    }
+
+    println!("{}", count);
 }
