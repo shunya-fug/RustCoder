d7fa4e4a9d53a12644ecbe066bc71fc27d4625d9
shunya-fug <shunnya0715@gmail.com>
2022-05-10 17:22:07 +0900

    solve: abc158_b

--- abc158/src/bin/b.rs
+++ abc158/src/bin/b.rs
@@ -2,5 +2,11 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {(n, a, b): (usize, usize, usize)}
+
+    let mut blue = a * (n / (a + b));
+    let m = n - (n / (a + b)) * (a + b);
+    blue += a.min(m);
+
+    println!("{}", blue);
 }
