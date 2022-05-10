d4f6894c96b1e1ca9047b12b77526c9ad4a2228d
shunya-fug <shunya0715@gmail.com>
2022-05-10 22:45:57 +0900

    solve: abc071_b

--- abc071/src/bin/b.rs
+++ abc071/src/bin/b.rs
@@ -1,6 +1,15 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {mut s: Chars}
+    s.sort_unstable();
+
+    for c in (b'a'..=b'z').map(char::from) {
+        if s.binary_search(&c).is_err() {
+            println!("{}", c);
+            return;
+        }
+    }
+    println!("None");
 }
