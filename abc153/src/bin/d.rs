8e2721b364525842af53835006bc1c276147d651
shunya-fug <shunnya0715@gmail.com>
2022-05-10 16:51:11 +0900

    solve: abc153_d

--- abc153/src/bin/d.rs
+++ abc153/src/bin/d.rs
@@ -2,5 +2,15 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {mut h: usize}
+
+    let mut attack: u128 = 0;
+    let mut monster: u128 = 1;
+    while h > 0 {
+        attack += monster;
+        monster *= 2;
+        h /= 2;
+    }
+
+    println!("{}", attack);
 }
