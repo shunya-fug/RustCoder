1e3a07246737805159149012b2c964e00c2797ee
shunya-fug <shunnya0715@gmail.com>
2022-05-10 18:15:56 +0900

    solve: abc063_b

--- abc063/src/bin/b.rs
+++ abc063/src/bin/b.rs
@@ -1,6 +1,13 @@
+use proconio::marker::Chars;
 use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {s: Chars}
+    let len = s.len();
+    let set: HashSet<char> = s.into_iter().collect();
+
+    let ans = if len == set.len() { "yes" } else { "no" };
+    println!("{}", ans);
 }
