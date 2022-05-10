18efd554cf100b17a60164f2d782bdc2de1524e3
shunya-fug <shunya0715@gmail.com>
2022-05-10 22:35:01 +0900

    solve: abc084_b

--- abc084/src/bin/b.rs
+++ abc084/src/bin/b.rs
@@ -1,6 +1,28 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+        s: Chars,
+    }
+
+    let ans = if s[a] == '-' {
+        let left = &s[..a];
+        let right = &s[a + 1..];
+
+        if left.len() == a
+            && right.len() == b
+            && left.iter().all(|c| ('0'..='9').contains(c))
+            && right.iter().all(|c| ('0'..='9').contains(c))
+        {
+            "Yes"
+        } else {
+            "No"
+        }
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
 }
