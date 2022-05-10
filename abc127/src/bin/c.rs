0ab385b04d20e3fda47c554dae6856a550f71d9a
shunya-fug <shunya0715@gmail.com>
2022-05-10 23:35:58 +0900

    solve: abc127_c

--- abc127/src/bin/c.rs
+++ abc127/src/bin/c.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, m): (usize, usize),
+        lr: [(usize, usize); m],
+    }
+
+    let mut l_max = 0;
+    let mut r_min = n;
+    for (l, r) in lr {
+        l_max = l_max.max(l);
+        r_min = r_min.min(r);
+    }
+
+    println!("{}", (r_min + 1).saturating_sub(l_max));
 }
