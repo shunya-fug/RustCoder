8f8539efcea933ffdb0fbc531e2a2ce8558301cc
shunya-fug <shunnya0715@gmail.com>
2022-05-11 12:34:38 +0900

    solve: abc123_b

--- abc123/src/bin/b.rs
+++ abc123/src/bin/b.rs
@@ -1,6 +1,25 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        dish: [usize; 5],
+    }
+
+    let mut ans = std::usize::MAX;
+    for order in (0..5).permutations(5) {
+        let mut t = 0;
+        for i in &order[..4] {
+            t += if dish[*i] % 10 == 0 {
+                dish[*i]
+            } else {
+                10 * (dish[*i] / 10 + 1)
+            }
+        }
+        t += dish[*order.last().unwrap()];
+        ans = ans.min(t);
+    }
+
+    println!("{}", ans);
 }
