3e774861aee4ac4afdd72d8fc9f49a46230a6a12
shunya-fug <shunnya0715@gmail.com>
2022-05-11 15:53:46 +0900

    solve: abc044_b

--- abc044/src/bin/b.rs
+++ abc044/src/bin/b.rs
@@ -1,6 +1,19 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
+use std::collections::HashMap;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {w: Chars}
+
+    let mut counter: HashMap<char, usize> = HashMap::new();
+    for c in w {
+        let count = counter.entry(c).or_insert(0);
+        *count += 1;
+    }
+
+    if counter.values().all(|n| n % 2 == 0) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
