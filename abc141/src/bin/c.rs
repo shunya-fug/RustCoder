64109cdb0dc3e1115b8a85c565671928f72f8c2c
shunya-fug <shunnya0715@gmail.com>
2022-05-11 11:07:20 +0900

    solve: abc141_c

--- abc141/src/bin/c.rs
+++ abc141/src/bin/c.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, k, q): (usize, isize, usize),
+        a: [usize; q],
+    }
+
+    let mut participant: Vec<isize> = vec![k - q as isize; n];
+    for a in a {
+        participant[a - 1] += 1;
+    }
+
+    for p in participant {
+        let result = if p > 0 { "Yes" } else { "No" };
+        println!("{}", result);
+    }
 }
