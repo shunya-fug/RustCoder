790e1e32ca157355a33729c9d9b53b720abedffc
shunya-fug <shunnya0715@gmail.com>
2022-05-11 15:47:38 +0900

    solve: abc118_b

--- abc118/src/bin/b.rs
+++ abc118/src/bin/b.rs
@@ -1,6 +1,22 @@
+use std::collections::HashSet;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, m): (usize, usize),
+    }
+    let mut food: HashSet<usize> = (1..=m).collect();
+
+    for _ in 0..n {
+        input! {k: usize, a: [usize; k]}
+
+        food = food
+            .intersection(&a.into_iter().collect::<HashSet<usize>>())
+            .cloned()
+            .collect();
+    }
+
+    println!("{}", food.len());
 }
