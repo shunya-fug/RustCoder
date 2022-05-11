4bc2e3cec666a3f40f8b9021f9ba5ef1b722a48b
shunya-fug <shunnya0715@gmail.com>
2022-05-11 15:06:57 +0900

    solve: agc002_a

--- agc002/src/bin/a.rs
+++ agc002/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {(a, b): (isize, isize)}
+
+    if a <= 0 && 0 <= b {
+        println!("Zero");
+    } else {
+        let negative: usize = (a..=b).filter(|x| *x < 0).count();
+        if negative % 2 == 0 {
+            println!("Positive");
+        } else {
+            println!("Negative");
+        }
+    }
 }
