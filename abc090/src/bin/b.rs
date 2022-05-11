e178f9bc3336a3a8e2b039dd63549ff7530f0de5
shunya-fug <shunnya0715@gmail.com>
2022-05-11 16:43:27 +0900

    solve: abc090_b

--- abc090/src/bin/b.rs
+++ abc090/src/bin/b.rs
@@ -2,5 +2,14 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {(a, b): (usize, usize)}
+
+    let ans = (a..=b)
+        .filter(|n| {
+            let s = n.to_string();
+            s == s.chars().rev().collect::<String>()
+        })
+        .count();
+
+    println!("{}", ans);
 }
