06ca689ea35f3eedd81984042f615af83219d656
shunya-fug <shunnya0715@gmail.com>
2022-05-11 17:33:53 +0900

    solve: abc079_b

--- abc079/src/bin/b.rs
+++ abc079/src/bin/b.rs
@@ -2,5 +2,15 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {n: usize}
+
+    let mut lucas_number: Vec<u128> = vec![0; n + 1];
+    lucas_number[0] = 2;
+    lucas_number[1] = 1;
+
+    for i in 2..=n {
+        lucas_number[i] = lucas_number[i - 1] + lucas_number[i - 2];
+    }
+
+    println!("{}", lucas_number.last().unwrap());
 }
