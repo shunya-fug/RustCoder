0222eb1c13f3f55b1d9bc2eb1c35059685ac6f5d
shunya-fug <shunnya0715@gmail.com>
2022-05-11 14:20:35 +0900

    solve: abc134_c

--- abc134/src/bin/c.rs
+++ abc134/src/bin/c.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {n: usize, mut a: [usize; n]}
+    let mut a_sorted = a.clone();
+    a_sorted.sort_unstable();
+    let amax = a_sorted.pop().unwrap();
+    if a.iter().filter(|a| **a == amax).count() == 1 {
+        let amax2nd = a_sorted.pop().unwrap();
+        for a in a {
+            println!("{}", if a == amax { amax2nd } else { amax });
+        }
+    } else {
+        for _ in a {
+            println!("{}", amax);
+        }
+    }
 }
