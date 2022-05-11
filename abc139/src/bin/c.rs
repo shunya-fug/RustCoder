8d55f8cf07a0f28fce41c782aab92dae25458a7d
shunya-fug <shunnya0715@gmail.com>
2022-05-11 18:29:47 +0900

    solve: abc139_c

--- abc139/src/bin/c.rs
+++ abc139/src/bin/c.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {n: usize, h: [usize; n]}
+
+    let mut ans = 0;
+    let mut count = 0;
+    for i in 0..n - 1 {
+        if h[i + 1] <= h[i] {
+            count += 1;
+        } else {
+            ans = ans.max(count);
+            count = 0;
+        }
+    }
+    ans = ans.max(count);
+
+    println!("{}", ans);
 }
