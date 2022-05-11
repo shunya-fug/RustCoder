8cebda1aaeab1d436da2b7ea91d613cdfd0ca64e
shunya-fug <shunnya0715@gmail.com>
2022-05-11 16:17:12 +0900

    solve: abc124_c

--- abc124/src/bin/c.rs
+++ abc124/src/bin/c.rs
@@ -1,6 +1,14 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {s: Chars}
+
+    let target1: Vec<char> = ['0', '1'].iter().cycle().take(s.len()).cloned().collect();
+    let target2: Vec<char> = ['1', '0'].iter().cycle().take(s.len()).cloned().collect();
+
+    let change1 = target1.iter().zip(&s).filter(|(a, b)| a != b).count();
+    let change2 = target2.iter().zip(&s).filter(|(a, b)| a != b).count();
+
+    println!("{}", change1.min(change2));
 }
