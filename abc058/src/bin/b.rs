2280532206866f33e660ea593eef024676445e4f
shunya-fug <shunya0715@gmail.com>
2022-05-12 22:10:40 +0900

    solve: abc058_b

--- abc058/src/bin/b.rs
+++ abc058/src/bin/b.rs
@@ -1,6 +1,10 @@
-use proconio::{fastout, input};
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {(o, e): (Chars, Chars)}
+
+    let ans: String = o.into_iter().interleave(e).collect();
+    println!("{}", ans);
 }
