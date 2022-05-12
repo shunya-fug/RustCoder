0a19df86736fa4a83c7913afbb2a30e23e4c4d90
shunya-fug <shunnya0715@gmail.com>
2022-05-12 15:15:12 +0900

    solve: abc061_b

--- abc061/src/bin/b.rs
+++ abc061/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, m): (usize, usize),
+        ab: [(usize, usize); m],
+    }
+    let mut road = vec![0; n + 1];
+    for (a, b) in ab {
+        road[a] += 1;
+        road[b] += 1;
+    }
+
+    for road in &road[1..] {
+        println!("{}", road);
+    }
 }
