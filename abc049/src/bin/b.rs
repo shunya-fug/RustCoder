2bfef08792b5739ed413b4686e503c1ee46432d3
shunya-fug <shunya0715@gmail.com>
2022-05-12 21:55:36 +0900

    solve: abc049_b

--- abc049/src/bin/b.rs
+++ abc049/src/bin/b.rs
@@ -2,5 +2,13 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (h, _): (usize, usize),
+        c: [String; h],
+    }
+
+    for c in c {
+        println!("{}", c);
+        println!("{}", c);
+    }
 }
