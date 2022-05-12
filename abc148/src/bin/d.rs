f8a24be03118228ede0671828487f22cf0df337b
shunya-fug <shunya0715@gmail.com>
2022-05-12 22:05:04 +0900

    solve: abc148_d

--- abc148/src/bin/d.rs
+++ abc148/src/bin/d.rs
@@ -2,5 +2,24 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut count = 0;
+    let mut target = 1;
+    for a in &a {
+        if *a == target {
+            target += 1;
+        } else {
+            count += 1;
+        }
+    }
+
+    if count == a.len() {
+        println!("{}", -1);
+    } else {
+        println!("{}", count);
+    }
 }
