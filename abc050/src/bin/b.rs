a095d4f9e69a47119cd9e7fd67cf8f43af8be679
shunya-fug <shunya0715@gmail.com>
2022-05-12 21:43:44 +0900

    solve: abc050_b

--- abc050/src/bin/b.rs
+++ abc050/src/bin/b.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        t: [isize; n],
+        m: usize,
+        px: [(usize, isize); m],
+    }
+
+    let solve_all_problem: isize = t.iter().sum();
+    for (p, x) in px {
+        let result = solve_all_problem - (t[p - 1] - x);
+        println!("{}", result);
+    }
 }
