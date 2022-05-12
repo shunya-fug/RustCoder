c1fd14b256a13a960811bc742efa7f82efa792f7
shunya-fug <shunnya0715@gmail.com>
2022-05-12 15:50:45 +0900

    solve: abc109_b

--- abc109/src/bin/b.rs
+++ abc109/src/bin/b.rs
@@ -1,6 +1,26 @@
-use proconio::{fastout, input};
+use std::collections::HashSet;
+
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {n: usize, w: [Chars; n]}
+    let w_set: HashSet<Vec<char>> = w.iter().cloned().collect();
+
+    let mut ans = "Yes";
+    if w_set.len() != w.len() {
+        ans = "No";
+    } else {
+        let mut last = *w[0].first().unwrap();
+
+        for w in w {
+            if last != *w.first().unwrap() {
+                ans = "No";
+                break;
+            }
+            last = *w.last().unwrap();
+        }
+    }
+
+    println!("{}", ans);
 }
