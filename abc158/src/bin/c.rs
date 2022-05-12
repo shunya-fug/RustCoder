eb16bbbe725e3b4ece15b5cdb2f2f1d4beb9f0f2
shunya-fug <shunnya0715@gmail.com>
2022-05-12 17:49:31 +0900

    solve: abc158_c

--- abc158/src/bin/c.rs
+++ abc158/src/bin/c.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {(a, b): (f64, f64)}
+
+    let mut ans: f64 = -1.0;
+    for n in 1..=1000 {
+        let n = n as f64;
+        if (n * 1.08).floor() == (n + a) && (n * 1.1).floor() == (n + b) {
+            ans = n;
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
