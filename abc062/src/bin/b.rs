f1837f1c4a4075b162d373e6e91ff766d5710823
shunya-fug <shunnya0715@gmail.com>
2022-05-12 11:23:16 +0900

    solve: abc062_b

--- abc062/src/bin/b.rs
+++ abc062/src/bin/b.rs
@@ -1,6 +1,27 @@
+use itertools::Itertools;
+use ndarray::{s, Array, Array2, Axis};
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (h, w): (usize, usize),
+        a: [String; h],
+    }
+
+    let mut base: Array2<char> = Array::from_elem((h + 2, w + 2), '#');
+    let a: Array2<char> = Array::from_shape_vec(
+        (h, w),
+        a.into_iter()
+            .flat_map(|s| s.chars().collect_vec())
+            .collect_vec(),
+    )
+    .unwrap();
+
+    base.slice_mut(s![1..=h, 1..=w]).assign(&a);
+    let ans = base;
+
+    for row in ans.axis_iter(Axis(0)) {
+        println!("{}", row.iter().collect::<String>());
+    }
 }
