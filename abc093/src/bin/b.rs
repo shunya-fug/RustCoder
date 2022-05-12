46892c9da9a64060b0d8bf0c60b98924815ff0f7
shunya-fug <shunnya0715@gmail.com>
2022-05-12 17:08:31 +0900

    solve: abc093_b

--- abc093/src/bin/b.rs
+++ abc093/src/bin/b.rs
@@ -1,6 +1,21 @@
 use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {(a, b, k): (isize, isize, isize)}
+    let mut register: HashSet<isize> = HashSet::new();
+
+    for n in a..(a + k).min(b) {
+        if !register.contains(&n) {
+            register.insert(n);
+            println!("{}", n);
+        }
+    }
+    for n in (b - k + 1).max(a)..=b {
+        if !register.contains(&n) {
+            register.insert(n);
+            println!("{}", n);
+        }
+    }
 }
