165798d3454a813b8db6f466d3e285bb7f2d8b46
shunya-fug <shunya0715@gmail.com>
2022-05-12 20:01:56 +0900

    solve: abc104_b

--- abc104/src/bin/b.rs
+++ abc104/src/bin/b.rs
@@ -1,6 +1,23 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {s: Chars}
+
+    let mut ans = "AC".to_string();
+
+    if s.first() != Some(&'A') {
+        ans = "WA".to_string();
+    }
+    if s[2..=s.len() - 2].iter().filter(|c| **c == 'C').count() != 1 {
+        ans = "WA".to_string();
+    }
+    if !s
+        .into_iter()
+        .all(|c| c == 'A' || c == 'C' || ('a'..='z').contains(&c))
+    {
+        ans = "WA".to_string();
+    }
+
+    println!("{}", ans);
 }
