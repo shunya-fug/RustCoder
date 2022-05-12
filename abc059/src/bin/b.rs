81df0d36bdbb2b2d5a1ffb5d8dda8dcf29e13c73
shunya-fug <shunnya0715@gmail.com>
2022-05-12 11:35:57 +0900

    solve: abc059_b

--- abc059/src/bin/b.rs
+++ abc059/src/bin/b.rs
@@ -1,6 +1,27 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
+use std::cmp::Ordering;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {(a, b): (Chars, Chars)}
+
+    let mut ans = "EQUAL";
+    match a.len().cmp(&b.len()) {
+        Ordering::Greater => ans = "GREATER",
+        Ordering::Less => ans = "LESS",
+        Ordering::Equal => {
+            for (a, b) in a.iter().zip(b) {
+                if a > &b {
+                    ans = "GREATER";
+                    break;
+                }
+                if a < &b {
+                    ans = "LESS";
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
