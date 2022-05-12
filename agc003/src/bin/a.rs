6559fe6e9a8473c3e707444ac34624c6aa2e76ac
shunya-fug <shunya0715@gmail.com>
2022-05-12 22:34:30 +0900

    solve: agc003

--- agc003/src/bin/a.rs
+++ agc003/src/bin/a.rs
@@ -1,6 +1,19 @@
-use proconio::{fastout, input};
+use im_rc::HashSet;
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {s: Chars}
+    let direction: HashSet<char> = s.into_iter().collect();
+
+    let ans = if direction.len() % 2 == 0
+        && ((direction.contains(&'N') && direction.contains(&'S'))
+            || (direction.contains(&'W') && direction.contains(&'E')))
+    {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
 }
