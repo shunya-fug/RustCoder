d032d33d8368e785e5cd7bb5a07bfaa2a03afb19
shunya-fug <shunnya0715@gmail.com>
2022-05-12 15:02:38 +0900

    solve: abc140_c

--- abc140/src/bin/c.rs
+++ abc140/src/bin/c.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut b: [usize; n-1]
+    }
+    b.push(std::usize::MAX);
+    let mut a = vec![0; n];
+    a[0] = b[0];
+
+    for (i, b) in b.windows(2).enumerate().rev() {
+        a[i + 1] = *b.iter().min().unwrap();
+    }
+
+    println!("{}", a.into_iter().sum::<usize>());
 }
