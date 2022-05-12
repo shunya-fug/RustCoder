46697780d76899a5df71df4d8a1f83cb144f7f99
shunya-fug <shunya0715@gmail.com>
2022-05-12 21:52:25 +0900

    solve: abc103_b

--- abc103/src/bin/b.rs
+++ abc103/src/bin/b.rs
@@ -1,6 +1,21 @@
-use proconio::{fastout, input};
+use std::collections::VecDeque;
+
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {(s, t): (Chars, Chars)}
+    let mut s = VecDeque::from(s);
+    let t = VecDeque::from(t);
+
+    let mut ans = "No";
+    for _ in 0..s.len() {
+        s.rotate_right(1);
+        if s == t {
+            ans = "Yes";
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
