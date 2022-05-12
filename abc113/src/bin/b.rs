09e09295291981b8b9a4fd95fd6757990924e477
shunya-fug <shunnya0715@gmail.com>
2022-05-12 18:13:07 +0900

    solve: abc113_b

--- abc113/src/bin/b.rs
+++ abc113/src/bin/b.rs
@@ -1,6 +1,19 @@
+use itertools::{Itertools, MinMaxResult};
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        (t, a): (f64, f64),
+        h: [f64; n],
+    }
+
+    let t = h.into_iter().map(|x| t - 0.006 * x).map(|t| (t - a).abs());
+    let ans = match t.position_minmax() {
+        MinMaxResult::MinMax(x, _) => x + 1,
+        _ => panic!(),
+    };
+
+    println!("{}", ans);
 }
