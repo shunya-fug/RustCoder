de98bae120c824a3c74038f870efcfd544a022b9
shunya-fug <shunya0715@gmail.com>
2022-05-12 22:17:52 +0900

    solve: abc053_b

--- abc053/src/bin/b.rs
+++ abc053/src/bin/b.rs
@@ -1,6 +1,14 @@
-use proconio::{fastout, input};
+use std::collections::VecDeque;
+
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {s: Chars}
+    let mut s = VecDeque::from(s);
+
+    while s.pop_front().unwrap() != 'A' {}
+    while s.pop_back().unwrap() != 'Z' {}
+
+    println!("{}", s.len() + 2);
 }
