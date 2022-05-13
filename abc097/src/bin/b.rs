70e97f9f976f9412b0dc1d5e29cbe78243af0258
shunya-fug <shunnya0715@gmail.com>
2022-05-13 16:29:57 +0900

    solve: abc097_b

--- abc097/src/bin/b.rs
+++ abc097/src/bin/b.rs
@@ -1,6 +1,30 @@
+use num::pow;
 use proconio::{fastout, input};
+use std::vec;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {x: usize}
+    let mut perfect_power = vec![false; x + 1];
+    perfect_power[1] = true;
+    for i in 2..x {
+        if i * 2 > x {
+            break;
+        }
+
+        for p in 2.. {
+            let n = pow(i, p);
+            if n > x {
+                break;
+            }
+            perfect_power[n] = true;
+        }
+    }
+
+    for n in (1..=x).rev() {
+        if perfect_power[n] {
+            println!("{}", n);
+            break;
+        }
+    }
 }
