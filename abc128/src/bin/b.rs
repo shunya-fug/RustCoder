1d68f380f6b1dbb4c7eb5ee75ae2f831bac5d7cb
shunya-fug <shunnya0715@gmail.com>
2022-05-13 15:24:34 +0900

    solve: abc128_b

--- abc128/src/bin/b.rs
+++ abc128/src/bin/b.rs
@@ -1,6 +1,24 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
+use std::cmp::Ordering::*;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        restaurant: [(String, usize); n],
+    }
+    let mut restaurant = restaurant
+        .into_iter()
+        .enumerate()
+        .map(|(i, r)| (r.0, r.1, i + 1))
+        .collect_vec();
+    restaurant.sort_by(|a, b| match a.0.cmp(&b.0) {
+        Less | Greater => a.0.cmp(&b.0),
+        Equal => a.1.cmp(&b.1).reverse(),
+    });
+
+    for (_, _, i) in restaurant.iter() {
+        println!("{}", i);
+    }
 }
