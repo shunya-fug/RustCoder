6bae77f27d94b64ddada185e14c29928d6e47c18
shunya-fug <shunnya0715@gmail.com>
2022-05-13 15:01:42 +0900

    solve: abc155_c

--- abc155/src/bin/c.rs
+++ abc155/src/bin/c.rs
@@ -1,6 +1,26 @@
 use proconio::{fastout, input};
+use std::collections::HashMap;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {n: usize, s: [String; n]}
+    let mut counter: HashMap<String, usize> = HashMap::new();
+
+    for s in s {
+        let count = counter.entry(s).or_insert(0);
+        *count += 1;
+    }
+    let count_max = counter.values().max().unwrap();
+
+    let mut counter: Vec<String> = counter
+        .iter()
+        .filter(|(_, c)| *c == count_max)
+        .map(|(s, _)| s)
+        .cloned()
+        .collect();
+    counter.sort();
+
+    for s in counter {
+        println!("{}", s);
+    }
 }
