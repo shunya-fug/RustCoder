ecc38da4844f09061d619c6c3ce804ba30582ac1
shunya-fug <shunya0715@gmail.com>
2022-05-14 21:07:18 +0900

    solve: abc251_a

--- abc251/src/bin/a.rs
+++ abc251/src/bin/a.rs
@@ -2,5 +2,14 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {s: String}
+
+    let ans = match s.len() {
+        1 => s.repeat(6),
+        2 => s.repeat(3),
+        3 => s.repeat(2),
+        _ => panic!(),
+    };
+
+    println!("{}", ans);
 }
