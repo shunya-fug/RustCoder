4db44af05923080a8d76a6c5db050ccfa8ba207d
shunya-fug <shunya0715@gmail.com>
2022-05-14 21:42:50 +0900

    solve: abc251_c

--- abc251/src/bin/c.rs
+++ abc251/src/bin/c.rs
@@ -1,6 +1,32 @@
-use proconio::{fastout, input};
+use proconio::{derive_readable, fastout, input};
+use std::collections::HashSet;
+
+#[derive_readable]
+struct Poem {
+    s: String,
+    t: usize,
+}
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        poem: [Poem; n],
+    }
+
+    let mut ans = 0;
+    let mut best = &poem[0];
+    let mut submitted: HashSet<String> = HashSet::new();
+    for (i, poem) in poem.iter().enumerate() {
+        if !submitted.contains(&poem.s) {
+            // original
+            if best.t < poem.t {
+                best = poem;
+                ans = i;
+            }
+        }
+        submitted.insert(poem.s.clone());
+    }
+
+    println!("{}", ans + 1);
 }
