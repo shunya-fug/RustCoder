0406ccc967d2b517c7c76b6d04f300896bab2e16
shunya-fug <shunya0715@gmail.com>
2022-05-14 21:26:02 +0900

    solve: abc251_b

--- abc251/src/bin/b.rs
+++ abc251/src/bin/b.rs
@@ -1,6 +1,33 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, w): (usize, usize),
+        mut a: [usize; n]
+    }
+    a.sort_unstable();
+
+    let mut good = vec![false; w + 1];
+    for n in &a {
+        if let Some(v) = good.get_mut(*n) {
+            *v = true;
+        }
+    }
+    for c in a.iter().combinations(2) {
+        let n: usize = c.into_iter().sum();
+        if let Some(v) = good.get_mut(n) {
+            *v = true;
+        }
+    }
+    for c in a.iter().combinations(3) {
+        let n: usize = c.into_iter().sum();
+        if let Some(v) = good.get_mut(n) {
+            *v = true;
+        }
+    }
+
+    let ans = good.into_iter().filter(|v| *v).count();
+    println!("{}", ans);
 }
