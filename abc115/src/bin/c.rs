bf5e7ece23cd23de6b6a0d22f8cca0154e4e68f4
shunya-fug <shunnya0715@gmail.com>
2022-05-16 18:10:44 +0900

    solve: abc115_c

--- abc115/src/bin/c.rs
+++ abc115/src/bin/c.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, k): (usize, usize),
+        mut h: [usize; n]
+    }
+    h.sort_unstable();
+
+    let ans: usize = h
+        .windows(k)
+        .map(|section| *section.last().unwrap() - *section.first().unwrap())
+        .min()
+        .unwrap();
+
+    println!("{}", ans);
 }
