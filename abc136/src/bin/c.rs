38fb7f92ddcb912e0f360890effe70fad8a002b2
shunya-fug <shunnya0715@gmail.com>
2022-05-17 14:19:54 +0900

    solve: abc136_c

--- abc136/src/bin/c.rs
+++ abc136/src/bin/c.rs
@@ -1,6 +1,25 @@
+use std::cmp::Ordering;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut h: [isize; n]
+    }
+
+    let mut ans = "Yes";
+    for i in (0..n).rev().skip(1) {
+        match h[i].cmp(&(h[i + 1] + 1)) {
+            Ordering::Equal => h[i] -= 1,
+            Ordering::Greater => {
+                ans = "No";
+                break;
+            }
+            _ => continue,
+        }
+    }
+
+    println!("{}", ans);
 }
