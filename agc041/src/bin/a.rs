44defaccb385123558fc3b55797fa4aefcc8d42c
shunya-fug <shunnya0715@gmail.com>
2022-05-17 14:38:42 +0900

    solve: agc041_a

--- agc041/src/bin/a.rs
+++ agc041/src/bin/a.rs
@@ -2,5 +2,13 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {(n, a, b): (usize, usize, usize)}
+
+    let ans = match (b - a) % 2 {
+        0 => (b - a) / 2,
+        1 => (a - 1).min(n - b) + 1 + (b - a - 1) / 2,
+        _ => panic!(),
+    };
+
+    println!("{}", ans);
 }
