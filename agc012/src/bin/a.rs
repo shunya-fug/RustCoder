1073a71c9e4331e49160287a73dea18ae4baf23b
shunya-fug <shunnya0715@gmail.com>
2022-05-17 13:14:26 +0900

    solve: agc012_a

--- agc012/src/bin/a.rs
+++ agc012/src/bin/a.rs
@@ -2,5 +2,13 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut a: [u128; 3 * n],
+    }
+    a.sort_unstable();
+
+    let ans: u128 = a[n..].iter().step_by(2).sum();
+
+    println!("{}", ans);
 }
