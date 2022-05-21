5bd0c41910f7d98cfc92fe5ee8a7802a3c46885d
shunya-fug <shunya0715@gmail.com>
2022-05-21 20:26:42 +0900

    solve: abc144_c

--- abc144/src/bin/c.rs
+++ abc144/src/bin/c.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {n: usize}
+
+    let mut ans = std::usize::MAX;
+    for i in 1..n {
+        if i * i > n {
+            break;
+        }
+
+        let j = n / i;
+        if i * j == n {
+            ans = ans.min((i - 1) + (j - 1));
+        }
+    }
+
+    println!("{}", ans);
 }
