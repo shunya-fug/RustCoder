5c8552960122c1bd1c6736234195df2555518e2d
shunya-fug <shunya0715@gmail.com>
2022-05-21 20:36:51 +0900

    solve: abc100_c

--- abc100/src/bin/c.rs
+++ abc100/src/bin/c.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        x: [usize; n]
+    }
+
+    let mut ans = 0;
+    for mut a in x {
+        while a % 2 == 0 {
+            ans += 1;
+            a /= 2;
+        }
+    }
+
+    println!("{}", ans);
 }
